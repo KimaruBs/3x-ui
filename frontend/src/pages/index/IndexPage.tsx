a7ee3e956cd5191749d46429b6f8ae74801fcf97
KimaruBs <kiriljaxla@gmail.com>
2026-07-07 15:24:27 +0300

    Update IndexPage.tsx

--- frontend/src/pages/index/IndexPage.tsx
+++ frontend/src/pages/index/IndexPage.tsx
@@ -140,7 +140,7 @@ export default function IndexPage() {
   }
 
   function openTelegram() {
-    window.open('https://t.me/XrayUI', '_blank', 'noopener,noreferrer');
+    window.open('https://t.me/KimaruBs', '_blank', 'noopener,noreferrer');
   }
 
   async function openConfig() {
